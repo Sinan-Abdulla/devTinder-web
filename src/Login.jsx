48fc10378c3548808d1adab7f4bba2e69b7e6ed0
sinan abdulla <sinanabdulla822@gmail.com>
2025-05-08 03:21:16 +0530

    hello 3

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+    return (
+        <div className='flex justify-center my-10'>
+            <div className="card bg-base-300 w-96 shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title flex justify-center">Login</h2>
+                    <div>
+                        <fieldset className="fieldset my-2">
+                            <legend className="fieldset-legend">Email ID</legend>
+                            <input type="text" className="input" placeholder="Enter your email" />
+                        </fieldset>
+                        <fieldset className="fieldset my-2">
+                            <legend className="fieldset-legend">Password</legend>
+                            <input type="text" className="input" placeholder="Enter your password" />
+                        </fieldset>
+                    </div>
+                    <div className="card-actions justify-center m-5">
+                        <button className="btn btn-primary" >Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
