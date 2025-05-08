58df30afb82f49f258c7c0ae278b8bff68fdea5a
sinan abdulla <sinanabdulla822@gmail.com>
2025-05-08 20:33:36 +0530

    hello 4

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [emailId, setEmailId] = useState("dhoni@gmail.com");
+    const [password, setPassword] = useState("Dhoni@123");
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:7777/login", {
+                email: emailId,
+                password: password
+            },
+                { withCredentials: true }
+            );
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
     return (
         <div className='flex justify-center my-10'>
             <div className="card bg-base-300 w-96 shadow-sm">
@@ -9,15 +27,27 @@ const Login = () => {
                     <div>
                         <fieldset className="fieldset my-2">
                             <legend className="fieldset-legend">Email ID</legend>
-                            <input type="text" className="input" placeholder="Enter your email" />
+                            <input
+                                type="text"
+                                value={emailId}
+                                className="input"
+                                placeholder="Enter your email"
+                                onChange={(e) => setEmailId(e.target.value)}
+                            />
                         </fieldset>
                         <fieldset className="fieldset my-2">
                             <legend className="fieldset-legend">Password</legend>
-                            <input type="text" className="input" placeholder="Enter your password" />
+                            <input
+                                type="text"
+                                value={password}
+                                className="input"
+                                placeholder="Enter your password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </fieldset>
                     </div>
                     <div className="card-actions justify-center m-5">
-                        <button className="btn btn-primary" >Login</button>
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
                     </div>
                 </div>
             </div>
