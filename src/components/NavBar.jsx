c02114783aa17148711b3fa3b955379d7f79b087
sinan abdulla <sinanabdulla822@gmail.com>
2025-05-10 23:07:23 +0530

    hello 6

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = memo(() => {
+  
   const user = useSelector((store) => store.user);
+  console.log(user);
+
 
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">🧑‍💻DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">🧑‍💻DevTinder</Link>
         </div>
 
         {user && (
@@ -29,10 +33,10 @@ const NavBar = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">Neww</span>
-                  </a>
+                  </Link>
                 </li>
                 <li><a>Settings</a></li>
                 <li><a>Logout</a></li>
@@ -44,6 +48,6 @@ const NavBar = () => {
       </div>
     </div>
   );
-};
+});
 
 export default NavBar
