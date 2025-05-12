9c9629d2e234c2aa95355b76c586b142548bfdec
sinan abdulla <sinanabdulla822@gmail.com>
2025-05-12 17:04:55 +0530

    hh

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
+    const { firstName, lastName, photoUrl, about, age, gender } = user;
     console.log(user);
-    
-    
+
+
 
 
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm ">
+        <div className="card bg-base-300 w-96 shadow-sm p-5">
             <figure>
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes" />
+                    src={user.photoUrl || "https://geographyandyou.com/images/user-profile.png"}
+                    alt="photo" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <h2 className="card-title">{firstName + " " + lastName}</h2>
+                {age && gender && <p>{age + "," + gender}</p>}
+                <p>{about}</p>
+                <div className="card-actions justify-center my-4">
+                    <button className="btn btn-primary">ignore</button>
+                    <button className="btn btn-secondary">interested</button>
                 </div>
             </div>
         </div>
