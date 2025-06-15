0bee43d6de9c0d15c4ce7928b8144293cad8d30a
sinan abdulla <sinanabdulla822@gmail.com>
2025-06-15 18:14:42 +0530

    k

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,8 @@
 import io from 'socket.io-client';
 import { BASE_URL } from './constants';
 
-
 export const createSocketConnection = () => {
-    if (localStorage.hostname === 'localhost') {
+    if (window.location.hostname === 'localhost') {
         return io(BASE_URL);
     } else {
         return io("/", { path: '/socket.io' });
