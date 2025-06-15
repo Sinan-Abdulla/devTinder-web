0fefbb57e4f60fac7aa7b812640bf8ef43ebafb2
sinan abdulla <sinanabdulla822@gmail.com>
2025-06-15 15:25:39 +0530

    chat

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,6 +2,10 @@ import io from 'socket.io-client';
 import { BASE_URL } from './constants';
 
 
-export const createSocketConnection = ()=>{
-    return io(BASE_URL);
+export const createSocketConnection = () => {
+    if (localStorage.hostname === 'localhost') {
+        return io(BASE_URL);
+    } else {
+        return io("/", { path: '/socket.io' });
+    }
 };
