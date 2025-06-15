e34416f4f11cfb68b1a0823894f5a42a91f2cacc
sinan abdulla <sinanabdulla822@gmail.com>
2025-06-15 21:08:53 +0530

    d

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,13 @@
 import io from 'socket.io-client';
-import { BASE_URL } from './constants';
+import { BASE_URL } from './constants'; // Make sure BASE_URL is "http://16.176.26.222"
 
 export const createSocketConnection = () => {
     if (window.location.hostname === 'localhost') {
         return io(BASE_URL);
     } else {
-        return io("/", { path: '/socket.io' });
+        return io("http://16.176.26.222", {
+            path: "/socket.io",
+            transports: ["websocket"], // forces WS connection
+        });
     }
 };
