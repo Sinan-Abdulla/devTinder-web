4591913931b03b37a71187a08df1249cda2d0417
sinan abdulla <sinanabdulla822@gmail.com>
2025-06-23 23:24:15 +0530

    s

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,18 +1,17 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'; 
 import { useDispatch, useSelector } from 'react-redux';
 import { BASE_URL } from '../utils/constants';
 import { addRequests, removeRequest } from '../utils/requestSlice';
 
-
 const Requests = () => {
   const dispatch = useDispatch();
-  const requests = useSelector((store) => store.request)
-
+  const requests = useSelector((store) => store.request);
+  const [loading, setLoading] = useState(true); 
   const reviewRequest = async (status, _id) => {
     try {
-      const res = await axios.post(
-        BASE_URL + "/request/review/" + status + "/" + _id,
+      await axios.post(
+        `${BASE_URL}/request/review/${status}/${_id}`,
         {},
         { withCredentials: true }
       );
@@ -22,43 +21,57 @@ const Requests = () => {
     }
   };
 
-
-
   const fetchRequest = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/user/requests/received", {
-        withCredentials: true
+      const res = await axios.get(`${BASE_URL}/user/requests/received`, {
+        withCredentials: true,
       });
       console.log(res.data.data);
-      dispatch(addRequests(res.data.data))
+      dispatch(addRequests(res.data.data));
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false); 
     }
   };
 
   useEffect(() => {
     fetchRequest();
-  }, [])
+  }, []);
 
-  if (!requests) return;
-  if (requests.length == 0)
+  if (loading) {
+    
     return (
-      <>
-        <h1 className="flex justify-center text-2xl my-10 text-green-300">
-          No requests found
-        </h1>
-      </>
+      <h1 className="flex justify-center text-2xl my-10 text-yellow-400">
+        Loading...
+      </h1>
     );
+  }
+
+  if (!requests || requests.length === 0) {
+    return (
+      <h1 className="flex justify-center text-2xl my-10 text-green-300">
+        No requests found
+      </h1>
+    );
+  }
 
   return (
-    <div className=" text-center my-10">
-      <h1 className="font-bold text-3xl text-pink-400">Requests ({requests.length})</h1>
+    <div className="text-center my-10">
+      <h1 className="font-bold text-3xl text-pink-400">
+        Requests ({requests.length})
+      </h1>
       {requests.map((request) => {
-        const { _id, firstName, lastName, photoUrl, age, gender, about } =
-          request.fromUserId;
+        const from = request?.fromUserId; 
+        if (!from) return null;           
+
+        const { _id, firstName, lastName, photoUrl, age, gender, about } = from;
 
         return (
-          <div key={_id} className="flex justify-between items-center m-4 p-4   rounded-lg bg-base-300 w-2/3  mx-auto">
+          <div
+            key={request._id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+          >
             <div>
               <img
                 alt="photo"
@@ -66,24 +79,32 @@ const Requests = () => {
                 src={photoUrl}
               />
             </div>
-            <div className="text-left m-4 p-4 ">
-              <h2 className="font-bold text-xl"> 
-                {firstName + " " + lastName}
+            <div className="text-left m-4 p-4">
+              <h2 className="font-bold text-xl">
+                {firstName + ' ' + lastName}
               </h2>
-              {age && gender && <p>{age + " " + gender}</p>}
+              {age && gender && <p>{age + ' ' + gender}</p>}
               <p>{about}</p>
             </div>
             <div>
-              <button className="btn btn-primary mx-2"
-                onClick={() => reviewRequest("rejected", request._id)}>Reject</button>
-              <button className="btn btn-secondary mx-2"
-                onClick={() => reviewRequest("accepted", request._id)}>accept</button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewRequest('rejected', request._id)}
+              >
+                Reject
+              </button>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => reviewRequest('accepted', request._id)}
+              >
+                Accept
+              </button>
             </div>
           </div>
         );
       })}
     </div>
   );
-}
+};
 
-export default Requests
+export default Requests;
